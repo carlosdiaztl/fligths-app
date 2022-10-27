d7cc062b32cf83fa68bc1e94b2d18c9688564aac
carlos diaz <108467585+carlosdiaztl@users.noreply.github.com>
2022-10-27 18:41:52 -0500

    formpage

--- src/components/ConfirmIDA/ConfirmIda.jsx
+++ src/components/ConfirmIDA/ConfirmIda.jsx
@@ -1,9 +1,150 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import '../ConfirmIDA/style.scss'
 const ConfirmIda = () => {
+  const [isTi, setIsTi] = useState(false);
+
+  const {
+      register,
+      handleSubmit,
+      formState: { errors },
+      watch,
+      setValue
+  } = useForm({
+      defaultValues: {
+          name: 'luis',
+          identificationType: ''
+      }
+  });
+
+  useEffect(() => {
+      //PETICION A LA API
+      setValue('age', 18)
+  }, [])
+
+  const onSubmit = (data) => {
+      console.log(data);
+  };
+
+  useEffect(() => {
+      if (watch('identificationType') === 'TI') {
+          setIsTi(true);
+      }else {
+          setIsTi(false);
+      }
+  }, [watch('identificationType')])
+
+
+  // const ValidatePass = (value) => {
+  //     if (value.length < 8) {
+  //         return "La contraseña debería contener al menos 8 caracteres"
+  //     } else if (value.length > 16) {
+  //         return "La contraseña debe contener menos de 16 de caracteres"
+  //     } else {
+  //         return true;
+  //     }
+  // }
+  const ValidateName = (value) => {
+      if (/^[A-Za-z]+$/i.test(value)) {
+          return true
+      } else {
+          return "El nombre ingresado contiene caracteres no permitidos"
+      }
+  }
+
   return (
-    <div>AQUI EL FORM</div>
-  )
-}
+    <div className="confirm">
+    <form onSubmit={handleSubmit(onSubmit)}>
+          <label>
+              Nombre:
+              <input
+                  type="text"
+                  placeholder="Escriba su nombre"
+                  {...register("name", { required: true, validate: ValidateName })}
+                  className={errors.name ? "input--error" : ""}
+              />
+              {errors.name && <span>{errors.name.message}</span>}
+          </label>
+          <label>
+              Teléfono:
+              <input
+                  type="number"
+                  placeholder="Escriba su número telefónico"
+                  {...register("phone", { required: true })}
+              />
+              {errors.phone && <span>Este campo es obligatorio</span>}
+          </label>
+          {/* <label>
+              Password:
+              <input
+                  type="password"
+                  placeholder="Escriba su contraseña"
+                  {...register("password", { required: true})}
+              />
+              {errors.password && <span>{errors.password.message}</span>}
+          </label> */}
+          <label>
+              Edad:
+              <input
+                  type="number"
+                  placeholder="Escriba su contraseña"
+                  {...register("age", { min: 18, max: 100 })}
+              />
+              {errors.age && <span>{errors.age.message}</span>}
+          </label>
+          <label>
+              Género:
+              <select
+                  {...register('gender')}
+              >
+                  <option value="">Seleccione su género</option>
+                  <option value="F">Femenino</option>
+                  <option value="M">Masculino</option>
+              </select>
+          </label>
+          <p>Tipo de documento</p>
+          <label>
+              <input
+                  type="radio"
+                  value="CC"
+                  {...register('identificationType')}
+              />
+              Cédula de Ciudadanía
+          </label>
+          <label>
+              <input
+                  type="radio"
+                  value="TI"
+                  {...register('identificationType')}
+              />
+              Tarjeta de Identidad
+          </label>
+          <label>
+              <input
+                  type="radio"
+                  value="NIT"
+                  {...register('identificationType')}
+              />
+              NIT
+          </label>
+          {
+              isTi && (
+                  <label>
+                      Número de TI:
+                      <input
+                          type="number"
+                          placeholder="Escriba su número de TI"
+                          {...register("identificationNumber")}
+                      />
+                      {errors.identificationNumber && <span>{errors.identificationNumber.message}</span>}
+                  </label>
+              )
+          }
+
+          <button type="submit">Guardar</button>
+      </form>
+     </div>
+  );
+};
 
 export default ConfirmIda
--- src/components/asientos/Asiento.jsx
+++ src/components/asientos/Asiento.jsx
@@ -1,96 +1,187 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "../asientos/style.scss";
 import Swal from "sweetalert2";
 import { Outlet, useNavigate } from "react-router-dom";
+import dollar from "../../icons/radio-button-checked/dollar-sign.svg";
 
 const Asiento = () => {
-  const navigate = useNavigate()
+  let asientos = [];
+
+  const navigate = useNavigate();
   const ticket = JSON.parse(sessionStorage.getItem("ticket"));
   console.log(ticket);
-  const [buttonGO, setbuttonGO] = useState(<></>)
 
   const [selectsit, setselectsit] = useState([]);
-  const listaA = ["A1", "A2", "A3", "A4",'A5','A6'];
-  const listaB = ["B1", "B2", "B3", "B4",'B5','B6'];
-  const listaC = ["C1", "C2", "C3", "C4",'C5','C6'];
-  let asientos = [];
-  const getAsiento = (asiento) => {
-    
-    if (asientos.length <= (ticket.sillasCompradas-1)) {
-    console.log(asiento);
-    console.log(ticket.sillasCompradas);
-    asientos.push(asiento);
-    }
-    console.log(asientos);
-      if (asientos.length === (ticket.sillasCompradas)) {
-        Swal.fire(
-          'GRACIAS',
-          `Por seleccionar sus sillas, confirme su compra  `,
-          'info'
-        )
-        setbuttonGO(<button onClick={GoRegister} > Confirmar compra </button>)
-        
-      }
-  };
-  const GoRegister=()=>{
-    navigate('/vuelos/asiento/confirm')
-    
+  const listaA = ["A1", "A2", "A3", "A4", "A5", "A6"];
+  const listaB = ["B1", "B2", "B3", "B4", "B5", "B6"];
+  const listaC = ["C1", "C2", "C3", "C4", "C5", "C6"];
 
-  }
+  const getAsiento = useCallback(
+    (asiento) => {
+      if (asientos.length <= ticket.sillasCompradas - 1) {
+        console.log(asiento);
+        console.log(ticket.sillasCompradas);
+        asientos.push(asiento);
+      }
+      console.log(asientos);
+
+      if (asientos.length === ticket.sillasCompradas) {
+        Swal.fire(
+          "GRACIAS",
+          `Por seleccionar sus sillas, confirme su compra  `,
+          "info"
+        );
+
+        setselectsit(asientos);
+      }
+    },
+    [asientos]
+  );
+
+  // const getAsiento = (asiento) => {
+
+  // };
+  const GoRegister = () => {
+    
+    console.log(selectsit);
+    const newCost = ticket.costofinal + ticket.costofinal * (19 / 100)
+    
+    const vueloConAsientos={
+     ...ticket,
+     costofinal:newCost,
+     sillas:selectsit
+    }
+    sessionStorage.clear()
+   sessionStorage.setItem('vuelocomprado',JSON.stringify(vueloConAsientos))
+    navigate('/vuelos/asiento/confirm')
+  };
+  const vaciar = () => {
+    setselectsit([]);
+  };
   return (
     <div className="mainseat">
-    
-      
-     
-
       <div className="div">
-     
-      
-     <div> 
-     </div>
+        <div></div>
         <section className="divjet">
-          {listaA.map((item, index) => (
-            <button
-              onClick={() => {
-                getAsiento(item);
-              }}
-              key={index}
-            >
-              {item}{" "}
-            </button>
-          ))}
+          {selectsit.length === ticket.sillasCompradas
+            ? listaA.map((item, index) => (
+                <button key={index}>
+                  {" "}
+                  Sillas ya elegidas
+                  {}{" "}
+                </button>
+              ))
+            : listaA.map((item, index) => (
+                <button
+                  onClick={() => {
+                    getAsiento(item);
+                  }}
+                  key={index}
+                >
+                  {item}{" "}
+                </button>
+              ))}
         </section>
-        <section className="divjet">
-          {listaB.map((item, index) => (
-            <button
-              onClick={() => {
-                getAsiento(item);
-              }}
-              key={index}
-            >
-              {item}{" "}
-            </button>
-          ))}
-        </section>
-        <section className="divjet">
-          {listaC.map((item, index) => (
-            <button
-              onClick={() => {
-                getAsiento(item);
-              }}
-              key={index}
-            >
-              {item}{" "}
-            </button>
-          ))}
-        </section>
-      </div>
-      <div className="mainseat_datos"> <h1>Aqui los datos </h1>
 
-      {buttonGO}
-      
+        <section className="divjet">
+          {selectsit.length === ticket.sillasCompradas
+            ? listaB.map((item, index) => (
+                <button key={index}>
+                  {" "}
+                  Sillas ya elegidas
+                  {}{" "}
+                </button>
+              ))
+            : listaB.map((item, index) => (
+                <button
+                  onClick={() => {
+                    getAsiento(item);
+                  }}
+                  key={index}
+                >
+                  {item}{" "}
+                </button>
+              ))}
+        </section>
+        <section className="divjet">
+          {selectsit.length === ticket.sillasCompradas
+            ? listaC.map((item, index) => (
+                <button key={index}>
+                  {" "}
+                  Sillas ya elegidas
+                  {}{" "}
+                </button>
+              ))
+            : listaC.map((item, index) => (
+                <button
+                  onClick={() => {
+                    getAsiento(item);
+                  }}
+                  key={index}
+                >
+                  {item}{" "}
+                </button>
+              ))}
+        </section>
       </div>
-      <Outlet/>
+      <div className="mainseat_datos">
+        {" "}
+        <h1>Datos generales</h1>
+        <section className="mainseat_datosvuelo">
+          <h3>Datos del vuelo</h3>
+
+          <span>Pasajeros: {ticket.sillasCompradas} </span>
+          <span> Vuelo de salida </span>
+          <span className="mainseat_spaninfo">
+            <section style={{ gap: "5px" }}>
+              <p> orgien:{ticket.origen} </p>
+              <p>Destino{ticket.destino} </p>
+            </section>
+            <section>
+              <p>Hora salida:{ticket.departureTime}Pm </p>
+              <p>Hora llegada:{ticket.departureTime + 2}Pm </p>
+            </section>
+            <span>Fecha:{ticket.salidaDate} </span>
+          </span>
+
+          <span>
+            {selectsit.map((item, index) => (
+              <p key={index}> Silla: {item} </p>
+            ))}{" "}
+          </span>
+        </section>
+        <section className="mainseat_spancosto">
+          <h3>Costo del vuelo</h3>
+
+          <span>
+            Costo total:{ticket.costofinal}{" "}
+            <img style={{ width: "18px" }} src={dollar} />
+          </span>
+          <span>
+            IVA: {ticket.costofinal * (19 / 100)}{" "}
+            <img style={{ width: "18px" }} src={dollar} />{" "}
+          </span>
+          <span>
+            Costo total + Iva :{" "}
+            {ticket.costofinal + ticket.costofinal * (19 / 100)}{" "}
+            <img style={{ width: "18px" }} src={dollar} />{" "}
+          </span>
+        </section>
+        {selectsit.length === ticket.sillasCompradas ? (
+          <button className="mainseat_datos_button1" onClick={vaciar}>
+            {" "}
+            Cambiar sillas{" "}
+          </button>
+        ) : (
+          ""
+        )}
+        {selectsit.length === ticket.sillasCompradas ? (
+          <button onClick={GoRegister}> Confirmar compra </button>
+        ) : (
+          ""
+        )}
+      </div>
+      <Outlet />
     </div>
   );
 };
